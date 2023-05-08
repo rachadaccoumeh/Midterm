23db15729221b23964407331bb7f8085a68ab83f
Rachad <rachadaccoumeh@gmailcom>
2023-05-08 18:36:38 +0300

    exam done

--- app/src/main/java/com/rachad/midterm/MainActivity.java
+++ app/src/main/java/com/rachad/midterm/MainActivity.java
@@ -6,50 +6,45 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-    EditText editTextNb1, editTextNb2,editTextPhone, editTextEmail;
-    Button buttonAdd, buttonCall, buttonOpenGoogle, buttonSendEmail;
+    EditText editTextPhone, editTextURL;
+    Button buttonCall, buttonOpenURL, buttonNext;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        editTextNb1 = findViewById(R.id.editTextNb1);
-        editTextNb2 = findViewById(R.id.editTextNb2);
-        buttonAdd = findViewById(R.id.buttonAdd);
+        //centre the appbar title
+
+
+
         buttonCall = findViewById(R.id.buttonCall);
-        buttonOpenGoogle = findViewById(R.id.buttonOpenGoogle);
-        buttonSendEmail = findViewById(R.id.buttonSendEmail);
+        buttonOpenURL = findViewById(R.id.buttonOpenURL);
         editTextPhone = findViewById(R.id.editTextPhoneNumber);
-        editTextEmail = findViewById(R.id.editTextEmail);
-        buttonAdd.setOnClickListener(v -> {
-            // move to another activity
-            Intent intent = new Intent(MainActivity.this, MainActivity2.class);
-            intent.putExtra("nb1", Integer.parseInt(editTextNb1.getText().toString()));
-            intent.putExtra("nb2", Integer.parseInt(editTextNb2.getText().toString()));
-            startActivity(intent);
-        });
+        editTextURL = findViewById(R.id.editTextURL);
+        buttonNext = findViewById(R.id.buttonNext);
+
         buttonCall.setOnClickListener(v -> {
             // call phone number
             Intent intent = new Intent(Intent.ACTION_DIAL);
             intent.setData(android.net.Uri.parse("tel:" + editTextPhone.getText().toString()));
             startActivity(intent);
         });
-        buttonOpenGoogle.setOnClickListener(v -> {
+        buttonOpenURL.setOnClickListener(v -> {
             // open google
-            Intent intent = new Intent(Intent.ACTION_VIEW);
-            intent.setData(android.net.Uri.parse("https://www.google.com"));
-            startActivity(intent);
+            try {
+
+                Intent intent = new Intent(Intent.ACTION_VIEW);
+                intent.setData(android.net.Uri.parse(editTextURL.getText().toString()));
+                startActivity(intent);
+            } catch (Exception e) {
+                e.printStackTrace();
+                Toast.makeText(this, "url is not correct", Toast.LENGTH_SHORT).show();
+            }
         });
-        buttonSendEmail.setOnClickListener(v -> {
-            // send email
-            Intent intent = new Intent(Intent.ACTION_SEND);
-            intent.setType("text/plain");
-            intent.putExtra(Intent.EXTRA_EMAIL, new String[]{editTextEmail.getText().toString()});
-            intent.putExtra(Intent.EXTRA_SUBJECT, "Subject");
-            intent.putExtra(Intent.EXTRA_TEXT, "Email body");
-            startActivity(intent);
-        });
+        buttonNext.setOnClickListener(v -> startActivity(new Intent(MainActivity.this, MainActivity2.class)));
+
     }
 }
--- app/src/main/java/com/rachad/midterm/MainActivity2.java
+++ app/src/main/java/com/rachad/midterm/MainActivity2.java
@@ -3,20 +3,78 @@ package com.rachad.midterm;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity2 extends AppCompatActivity {
     TextView textViewResult;
+    EditText editTextNb1, editTextNb2;
+    Button buttonAdd, buttonSub, buttonMul, buttonDiv;
+    Button buttonBack, buttonClose;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
         textViewResult = findViewById(R.id.textViewResult);
-        int nb1 = getIntent().getIntExtra("nb1", 0);
-        int nb2 = getIntent().getIntExtra("nb2", 0);
-        int result = nb1 + nb2;
-        textViewResult.setText(String.valueOf(result));
+        editTextNb1 = findViewById(R.id.editTextNb1);
+        editTextNb2 = findViewById(R.id.editTextNb2);
+        buttonAdd = findViewById(R.id.buttonAdd);
+        buttonSub = findViewById(R.id.buttonSub);
+        buttonMul = findViewById(R.id.buttonMultiply);
+        buttonDiv = findViewById(R.id.buttonDivide);
+        buttonBack = findViewById(R.id.buttonBack);
+        buttonClose = findViewById(R.id.buttonClose);
+        buttonBack.setOnClickListener(v -> {
+            finish();
+        });
+        buttonClose.setOnClickListener(v -> {
+            finishAffinity();
+        });
+        buttonAdd.setOnClickListener(v -> {
+            try {
+                float nb1 = Float.parseFloat(editTextNb1.getText().toString());
+                float nb2 = Float.parseFloat(editTextNb2.getText().toString());
+                textViewResult.setText(String.valueOf(nb1 + nb2));
+                textViewResult.setVisibility(View.VISIBLE);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        });
+        buttonSub.setOnClickListener(v -> {
+            try {
+                float nb1 = Float.parseFloat(editTextNb1.getText().toString());
+                float nb2 = Float.parseFloat(editTextNb2.getText().toString());
+                textViewResult.setText(String.valueOf(nb1 - nb2));
+                textViewResult.setVisibility(View.VISIBLE);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        });
+        buttonMul.setOnClickListener(v -> {
+            try {
+                float nb1 = Float.parseFloat(editTextNb1.getText().toString());
+                float nb2 = Float.parseFloat(editTextNb2.getText().toString());
+                textViewResult.setText(String.valueOf(nb1 * nb2));
+                textViewResult.setVisibility(View.VISIBLE);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        });
+        buttonDiv.setOnClickListener(v -> {
+            try {
+                float nb1 = Float.parseFloat(editTextNb1.getText().toString());
+                float nb2 = Float.parseFloat(editTextNb2.getText().toString());
+                textViewResult.setText(String.valueOf(nb1 / nb2));
+                textViewResult.setVisibility(View.VISIBLE);
+            } catch (Exception e) {
+                e.printStackTrace();
+                Toast.makeText(this, "division by zero", Toast.LENGTH_SHORT).show();
+            }
+        });
 
 
     }
